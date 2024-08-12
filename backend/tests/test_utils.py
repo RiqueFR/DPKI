454d3e392d01ee344dd59acdb26244f5f621a3c3
Henrique Faria <rique.far.rib@gmail.com>
2024-08-12 18:30:47 -0300

    add the invalid signed cert to tests

--- backend/tests/test_utils.py
+++ backend/tests/test_utils.py
@@ -8,8 +8,14 @@ def test_valid_signed_cert_return_true():
     assert check_certificate_valid(valid_cert_data) is True
 
 
-def test_invalid_signed_cert_return_false():
+def test_corrupted_signed_cert_return_false():
     valid_cert_data = None
     with open("tests/invalid_cert1.pem", "rb") as valid_cert_file:
+        valid_cert_data = valid_cert_file.read()
+    assert check_certificate_valid(valid_cert_data) is False
+
+def test_invalid_signed_cert_return_false():
+    valid_cert_data = None
+    with open("tests/invalid_cert2.pem", "rb") as valid_cert_file:
         valid_cert_data = valid_cert_file.read()
     assert check_certificate_valid(valid_cert_data) is False
