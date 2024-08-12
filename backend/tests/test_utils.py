1456f4618e4819f4009593e7767bdd30f042a357
Henrique Faria Ribeiro <rique.far.rib@gmail.com>
2024-08-11 23:05:19 -0300

    created validate cert function

---
+++ backend/tests/test_utils.py
@@ -0,0 +1,15 @@
+from backend.utils import check_certificate_valid
+
+
+def test_valid_signed_cert_return_true():
+    valid_cert_data = None
+    with open("tests/signed_cert1.pem", "rb") as valid_cert_file:
+        valid_cert_data = valid_cert_file.read()
+    assert check_certificate_valid(valid_cert_data) is True
+
+
+def test_invalid_signed_cert_return_false():
+    valid_cert_data = None
+    with open("tests/invalid_cert1.pem", "rb") as valid_cert_file:
+        valid_cert_data = valid_cert_file.read()
+    assert check_certificate_valid(valid_cert_data) is False
---
+++ backend/utils.py
@@ -0,0 +1,23 @@
+import base64
+
+from cryptography import x509
+
+
+def check_certificate_valid(signed_certificate):
+    cert = None
+    try:
+        cert = x509.load_pem_x509_certificate(signed_certificate)
+    except ValueError:
+        return False
+
+    public_key = cert.public_key()
+    signature = cert.signature
+    tbs_certificate_bytes = cert.tbs_certificate_bytes
+    hash_alg = cert.signature_hash_algorithm
+    padding = cert.signature_algorithm_parameters
+
+    try:
+        public_key.verify(signature, tbs_certificate_bytes, padding, hash_alg)
+        return True
+    except Exception:
+        return False
