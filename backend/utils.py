3ae9e72d543a0f40054b16764fb1b90fad293dd4
Milena0899 <milenamantovanelli7@gmail.com>
2024-08-19 00:11:33 -0300

    adding revoke in dapp

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,5 @@
 """Module used to create functions that help main program"""
+import json
 
 import sqlite3
 from cryptography import x509
@@ -11,13 +12,19 @@ table_name = "user_certificates"
 con = sqlite3.connect("data.db")
 cur = con.cursor()
 
+def dict_to_bytes(dict_data: dict):
+    """Convert python dict to bytes"""
+    json_str = json.dumps(dict_data)
+    return json_str.encode()
+
 def create_table():
     cur.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (user_id varchar(200), certificate varchar(1800), active bool, public_key varchar(200), due_data date)")
 
-def get_certificate_by_user():
+def get_certificate_by_user(user_id: str):
     try:
         cur.execute(f"SELECT * FROM {table_name} WHERE user_id = '{user_id}'")
-        return True
+        row = cur.fetchone()
+        return row
     except Exception as e:
         return False
 
@@ -34,6 +41,14 @@ def add_user_certificate(user_id: str, certificate: str):
         cur.execute(f"INSERT INTO {table_name} VALUES ({user_id}, {certificate}, true, {pub_key})")
         return True
     except Exception as e:
+        return False
+    
+def update_certificate_status(cert_new_status: bool, user_id: str):
+    try:
+        sql = 'UPDATE table_name SET active = ? WHERE user_id = ?'
+        cur.execute(sql, (cert_new_status, user_id))
+        return True
+    except:
         return False
 
 def get_certificate_pubkey(cert: str):
@@ -65,17 +80,3 @@ def check_signature_belongs_to_certificate_valid(
         return True
     except InvalidSignature:
         return False
-
-def revoke_certificate(signature: bytes, message:bytes, cert: str):
-    # cur.execute(f"SELECT * FROM {table_name} WHERE user_id = '{user_id}'")
-    # row = cur.fetchone()
-    user = get_certificate_by_user()
-    pub_key = get_certificate_pubkey(user[1])
-    try:
-        if check_signature_belongs_to_certificate_valid(message, pub_key, signature):
-            sql = 'UPDATE table_name SET active = ? WHERE user_id = ?'
-            cur.execute(sql, (False, user[0]))
-        else:
-            print("Invalid Signature!")
-    except Exception as e:
-        print("Revoke error")
